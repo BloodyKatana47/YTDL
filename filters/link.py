5a6785754b2cc1d0cac074ea2f872442eefaea86
bloodykatana47 <mirazizmir2005@gmail.com>
2024-05-26 12:00:36 +0500

    Fix regexp

--- filters/link.py
+++ filters/link.py
@@ -12,9 +12,14 @@ class IsCorrectLink(BoundFilter):
     key: str = 'is_correct_link'
 
     async def check(self, message: types.Message) -> Union[bool, Dict[str, List[str]]]:
-        pattern: str = (r'^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:['
-                        r'\w\-]+\?v=|embed\/|live\/|v\/|shorts\/)?)([\w\-]+)(\S+)?$')
-        pattern_match: str = re.match(pattern, message.text).group(5)
-        if len(pattern_match) == 11:
-            return {'matches': [pattern_match]}
-        return False
+        pattern1: str = r'(?:https?://)?(?:www\.)?youtube\.com/shorts/([\w\-]+)'
+        pattern1_matches: List[str] = re.findall(pattern1, message.text)
+
+        pattern2: str = r'(?:youtu\.be/|youtube\.com/watch\?v=)([\w\-]+)'
+        pattern2_matches: List[str] = re.findall(pattern2, message.text)
+
+        matches: List[str] = pattern1_matches + pattern2_matches
+        if len(matches) == 0:
+            return False
+        else:
+            return {'matches': matches}
