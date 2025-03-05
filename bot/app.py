b503fa15c3fb3311f1096ee1ac6c3b775262f868
BloodyKatana47 <mirazizmir2005@gmail.com>
2025-03-05 10:39:39 +0500

    Fix adding handlers

--- bot/app.py
+++ bot/app.py
@@ -3,12 +3,13 @@ from logging import INFO, basicConfig
 from aiogram import executor
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
+import handlers
 from bot import filters
 from loader import dp
 
 basicConfig(level=INFO)
 dp.middleware.setup(LoggingMiddleware())
+filters.setup(dp)
 
 if __name__ == '__main__':
-    filters.setup(dp)
     executor.start_polling(dp, skip_updates=True)
